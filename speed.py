dd2e0a86d8f81e5e2432e86b1d1825b99990ce6d
petersNikolA <peters.na@phystech.edu>
2020-12-04 10:08:17 +0300

    test code

---
+++ speed.py
@@ -0,0 +1,56 @@
+import pygame
+from pygame.draw import *
+
+
+class Speeder:
+
+    def __init__(self, x=50, y=200, l_1=20, l_2=50):
+        self.x = x
+        self.y = y
+        self.width = l_1
+        self.high = l_2
+        self.speed = 50
+
+    def draw(self):
+        rect(screen, (255, 255, 255), (int(self.x), self.y, self.width, self.high))
+
+    def move(self, dt):
+        self.x += self.speed * dt
+
+    def control(self):
+        if self.x >= -self.width + 650 or self.x <= 50:
+            self.speed *= -1
+
+    def check(self):
+        if 330 <= self.x <= -self.width + 370:
+            print(3)
+        elif 290 <= self.x <= -self.width + 410:
+            print(2)
+        else:
+            print(1)
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+finished = False
+FPS = 60
+t = 1 / FPS
+s = Speeder()
+while not finished:
+    rect(screen, (0, 0, 255), (50, 200, 600, 50))
+    rect(screen, (0, 255, 0), (290, 200, 120, 50))
+    rect(screen, (255, 0, 0), (330, 200, 40, 50))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.KEYDOWN:
+            s.check()
+    s.move(t)
+    s.control()
+    s.draw()
+    pygame.display.update()
+    screen.fill((0, 0, 0))
+
+pygame.quit()
